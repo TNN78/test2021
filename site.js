5fad6e45005d31f49df84a62ced0d9f583afb5a6
TRAN <ngocnhan_tran@yahoo.fr>
2019-10-18 20:57:15 +0200

    MOT DE PASSE

--- site.js
+++ site.js
@@ -6,3 +6,22 @@ $(window).scroll(function(){
     $(".navbar").addClass("fixed-top");
   }
 });
+function passWord() {
+var testV = 1;
+var pass1 = prompt('Veuillez entrer le mot de passe',' ');
+while (testV < 10) {
+if (!pass1) 
+history.go(-1);
+if (pass1.toLowerCase() == "gialongparis75") {
+window.location="content/TrangChinh.html";
+break ;
+} 
+testV+=1;
+var pass1 = 
+prompt('Accès interdit - Mot de passe incorrect. Veuillez réessayer.','Mot de passe');
+}
+if (pass1.toLowerCase()!="gialongparis75" & testV ==10) 
+history.go(-1);
+return " ";
+} 
+
