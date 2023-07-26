4d164692eec640836211dfe6715b340b609f51fe
TNN <54471029+TNN78@users.noreply.github.com>
2023-07-26 14:06:58 +0200

    Ghi Danh
    Ghi Danh

--- site.js
+++ site.js
@@ -103,12 +103,15 @@ function SelThongTin(indMenu) {
         var myImg = document.getElementById("imgtt");
         switch(indMenu) {
                 case 1:
-                  myImg.src='../GhiDanh/DSCNuSinh1970-1974.jpg';
+                  myImg.src='../GhiDanh/DSCNuSinh1960-1969.jpg';
                   break;
                 case 2:
-                  myImg.src='../GhiDanh/DSCNuSinh1975-1979.jpg';
+                  myImg.src='../GhiDanh/DSCNuSinh1970-1974.jpg';
                   break;
                 case 3:
+                  myImg.src='../GhiDanh/DSCNuSinh1975-1979.jpg';
+                  break;
+                case 4:
                   myImg.src='../GhiDanh/DSCNuSinh1980.jpg';
                   break;
             }
