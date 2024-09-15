1bee0538535d909b192f6e4c411f385eb213d41b
TNN <54471029+TNN78@users.noreply.github.com>
2024-09-15 15:26:11 +0200

    15-09-24
    15-09-24

--- site.js
+++ site.js
@@ -104,6 +104,9 @@ function SelThongTin(indMenu) {
             case 102:
               myImg.src='images/ThuNgoHT2023.jpg';
               break;
+            case 20:
+              myImg.src='images/TinGioChot.jpg';
+              break;
             case 19:
               myImg.src='images/ThongBao_09-2024.jpg';
               break;
